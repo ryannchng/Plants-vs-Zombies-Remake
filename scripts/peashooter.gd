17b41c258ba728cea4271a6fb70477a8bbb246d5
txxdy <ryanteddy1716@gmail.com>
2025-03-08 22:31:32 -0500

    Minor fixes

--- scripts/peashooter.gd
+++ scripts/peashooter.gd
@@ -1,17 +1,11 @@
 extends Node2D
 
-@export var pea_scene: PackedScene  # Reference to the Pea projectile scene
 
-@onready var sprite = $Sprite
-@onready var shooting_timer = $ShootingTimer
-@onready var muzzle = $Muzzle
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
 
-func _ready():
-	shooting_timer.timeout.connect(shoot)
-	shooting_timer.start()  # Start shooting timer
 
-func shoot():
-	sprite.play("shoot")  # Play shooting animation
-	var pea = pea_scene.instantiate() as Node2D
-	get_parent().add_child(pea)  # Add projectile to the main scene
-	pea.global_position = muzzle.global_position  # Spawn at muzzle
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
